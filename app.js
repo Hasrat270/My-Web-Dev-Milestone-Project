200b5d37a40a9aa8499bafef6ee78e805eeb0f82
Hasrat Afridi <hasrat3701@gmail.com>
2025-09-07 11:56:29 +0500

    My web dev Milestone Project day 3

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const authRoutes = require('./routes/auth.routes');
+const db = require("./data/database");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs'); // setting ejs
-app.set('views', path.join(__dirname, 'views')); // engine
+app.set("view engine", "ejs"); // setting ejs
+app.set("views", path.join(__dirname, "views")); // engine
 
-app.use(express.static('public')); // access static files
+app.use(express.static("public")); // access static files
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
-app.listen(3000, function() {
-    console.log('Server Running on : http://localhost:3000');
-});
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000, function () {
+      console.log("Server Listening on : http://localhost:3000/signup");
+    });
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to database!");
+    console.log(error);
+  });
