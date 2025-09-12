9057975d4130b0b44b248f4c762610c46b1c0ec1
Hasrat Afridi <hasrat3701@gmail.com>
2025-09-12 09:47:47 +0500

    My web dev Milestone Project day 4

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 const express = require("express");
+const csrf = require("csurf");
+const expressSession = require("express-session");
 
+const createSessionConfig = require("./config/session");
 const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
+const protectRoutesMiddleware = require("./middlewares/protect-routes");
+const cartMiddleware = require("./middlewares/cart");
 const authRoutes = require("./routes/auth.routes");
+const productRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
+const cartRoutes = require("./routes/cart.routes");
 
 const app = express();
 
@@ -11,9 +23,30 @@ app.set("view engine", "ejs"); // setting ejs
 app.set("views", path.join(__dirname, "views")); // engine
 
 app.use(express.static("public")); // access static files
+app.use("/products/assets", express.static("product-data"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(cartMiddleware); // Cart Middleware
+
+app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productRoutes);
+app.use("/cart", cartRoutes);
+app.use(protectRoutesMiddleware); // It will protect that routes
+// which are coming next in line , then if we are logged out we can
+// not access a route just by entering it in the url tab !
+app.use("/admin", adminRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
